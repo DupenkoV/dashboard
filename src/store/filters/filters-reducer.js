84ed2e2c1a2085cb4c093fd5153d4a96811288bb
Vladimir <Volor88@ya.ru>
2023-03-06 17:34:28 +0300

    added checking for duplicates

--- src/store/filters/filters-reducer.js
+++ src/store/filters/filters-reducer.js
@@ -3,10 +3,13 @@ import { ADD_FILTER, DELETE_ALL_FILTERS, DELETE_FILTER } from "./filters-constan
 export const filtersReducer = (state = [], action) => {
     switch(action.type) {
         case ADD_FILTER: {
-            return [
-                ...state,
-                action.payload
-            ]
+            if(!state.includes(action.payload)) {
+                return [
+                    ...state,
+                    action.payload
+                ]
+            }
+            return state
         }
         case DELETE_FILTER: {
             return state.filter(item => item !== action.payload)
